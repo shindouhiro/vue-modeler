19c84058a309fefdb1a0e5fc05d7f526ae8fd15a
hikaru <274173368@qq.com>
2022-04-07 15:56:43 +0800

    集成logicFlow拓展

--- src/pages/Modeler.vue
+++ src/pages/Modeler.vue
@@ -1,12 +1,20 @@
 <template>
-  <div ref="lfRef"></div>
+  <div ref="lfRef" h-screen></div>
 </template>
 <script lang="ts" setup>
 import { onMounted, ref, nextTick, unref } from 'vue'
+import {
+  Snapshot,
+  BpmnElement,
+  Menu,
+  DndPanel,
+  SelectionSelect
+} from '@logicflow/extension'
+
 import type { Ref } from 'vue'
 import LogicFlow from '@logicflow/core'
-// import '@logicflow/core/dist/style/index.css'
-// import '@logicflow/extension/lib/style/index.css'
+import '@logicflow/core/dist/style/index.css'
+import '@logicflow/extension/lib/style/index.css'
 const lf = ref(null) as Ref<LogicFlow | null>
 const lfRef = ref(null)
 const data = {
@@ -42,10 +50,28 @@ async function init() {
   if (!lfEl) {
     return
   }
+  LogicFlow.use(DndPanel)
+
+  // Canvas configuration
+  LogicFlow.use(Snapshot)
+  // Use the bpmn plug-in to introduce bpmn elements, which can be used after conversion in turbo
+  LogicFlow.use(BpmnElement)
+  // Start the right-click menu
+  LogicFlow.use(Menu)
+  LogicFlow.use(SelectionSelect)
+
   lf.value = new LogicFlow({
     container: lfEl,
-    width: 500,
-    height: 500
+    snapline: true,
+    grid: true,
+    stopScrollGraph: true,
+    stopZoomGraph: true,
+    background: {
+      color: '#F3F3F3'
+    },
+    keyboard: {
+      enabled: true
+    }
   })
   onRender()
 }
