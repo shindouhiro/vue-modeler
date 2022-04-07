a0053f2602d86cbb5ed538d9f1f4c53138084334
hikaru <274173368@qq.com>
2022-04-07 14:27:50 +0800

    修改unocss.config 为ts文件

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,7 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'path' {
+  export function resolve(...args: string[]): string
+}
