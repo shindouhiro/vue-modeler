9fcf78f278a6eef0790520d9c716db678d51c66d
hikaru <274173368@qq.com>
2022-04-08 15:33:43 +0800

    修改包信息

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Unocss from 'Unocss/vite'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'Unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
